e3cdfda0c645c6cb8ca7bf9bf019c29ba9f04c5f
Mike <mike@awesomebox.net>
2013-12-19 20:30:35 +0000

    upadated error handler to return 500

--- www/includes/globals.php
+++ www/includes/globals.php
@@ -3,6 +3,7 @@ set_exception_handler('exception_handler');
 
 function exception_handler($exception)
 {
+	http_response_code(500);	
 	echo $exception->getMessage(), ' in ', $exception->getFile(), ':', $exception->getCode();
 }
 ?>
